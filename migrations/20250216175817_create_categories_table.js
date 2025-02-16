5f160082e884cfe0b39d30fd7484b09b05a42af5
Amy <xyl.amy@gmail.com>
2025-02-16 13:11:59 -0800

    create schema for categories table

--- migrations/20250216175817_create_categories_table.js
+++ migrations/20250216175817_create_categories_table.js
@@ -2,14 +2,17 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
-};
+export function up(knex) {
+  return knex.schema.createTable("categories", (table) => {
+    table.increments("id").primary();
+    table.string("category_name").unique().notNullable();
+  });
+}
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
-};
+export function down(knex) {
+  return knex.schema.dropTable("categories");
+}
