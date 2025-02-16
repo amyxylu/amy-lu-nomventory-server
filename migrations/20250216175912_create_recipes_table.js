67b42a7047059ea00887437cc98eea663fb3a3f5
Amy <xyl.amy@gmail.com>
2025-02-16 14:17:20 -0800

    create schema for recipes table

--- migrations/20250216175912_create_recipes_table.js
+++ migrations/20250216175912_create_recipes_table.js
@@ -2,14 +2,33 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
-};
+export function up(knex) {
+  return knex.schema.createTable("recipes", (table) => {
+    table.increments("id").primary();
+    table.string("recipe_name").notNullable();
+    table.text("description").notNullable();
+    table.string("difficulty_level", 10).notNullable(); // Easy, Medium, Hard
+    table.integer("cook_time").unsigned().notNullable(); // Minutes
+    table.integer("servings").unsigned().notNullable();
+    table.text("instructions").notNullable();
+    table.string("image_url");
 
+    table
+      .integer("cuisine_id")
+      .unsigned()
+      .notNullable()
+      .references("id")
+      .inTable("cuisines")
+      .onUpdate("CASCADE")
+      .onDelete("CASCADE");
+
+    table.timestamps(true, true);
+  });
+}
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
-};
+export function down(knex) {
+  return knex.schema.dropTable("recipes");
+}
