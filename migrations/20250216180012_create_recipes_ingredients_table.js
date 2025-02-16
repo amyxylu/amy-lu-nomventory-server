4e31471c4804bf99e8b55d545a5d60500f882782
Amy <xyl.amy@gmail.com>
2025-02-16 14:28:13 -0800

    create schema for recipe_ingredients table

--- migrations/20250216180012_create_recipes_ingredients_table.js
+++ migrations/20250216180012_create_recipes_ingredients_table.js
@@ -2,14 +2,38 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
-};
+export function up(knex) {
+  return knex.schema.createTable("recipe_ingredients", (table) => {
+    table.increments("id").primary();
+
+    table
+      .integer("recipe_id")
+      .unsigned()
+      .notNullable()
+      .references("id")
+      .inTable("recipes")
+      .onUpdate("CASCADE")
+      .onDelete("CASCADE");
+
+    table
+      .integer("ingredient_id")
+      .unsigned()
+      .notNullable()
+      .references("id")
+      .inTable("ingredients")
+      .onUpdate("CASCADE")
+      .onDelete("CASCADE");
+
+    table.string("quantity").notNullable();
+
+    table.timestamps(true, true);
+  });
+}
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
-};
+export function down(knex) {
+  return knex.schema.dropTable("recipe_ingredients");
+}
