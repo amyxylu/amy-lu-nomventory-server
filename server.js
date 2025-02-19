743f689d568b26e0cc174f6951cd862b405420be
Amy <xyl.amy@gmail.com>
2025-02-18 22:15:53 -0800

    update BASE_URL logic, and file path directory for images

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 
-const { PORT, CORS_ORIGIN } = process.env;
+const { PORT, CORS_ORIGIN, BASE_URL } = process.env;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(cors({ origin: CORS_ORIGIN }));
 app.use(express.json());
-app.use("/images", express.static("public/images"));
+
+app.use("/images", express.static(path.join(__dirname, "public/images")));
 
 import ingredientsRoutes from "./routes/ingredients-routes.js";
 import categoriesRoutes from "./routes/categories-routes.js";
@@ -23,10 +29,10 @@ app.use("/api/recipes", recipesRoutes);
 app.use("/api/filters", filtersRoutes);
 
 app.get("/", (req, res) => {
-  res.send("Express is running!");
+  res.send(`Express is running! API Base URL: ${BASE_URL}`);
 });
 
-app.listen(PORT, function () {
-  console.log(`listening on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on ${BASE_URL}`);
   console.log(`CORS_ORIGIN: ${CORS_ORIGIN}`);
 });
